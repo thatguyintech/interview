66e946fd80dc4da93135287f21de189e85524d20
Albert Hu <alberthu@berkeley.edu>
2014-11-24 03:39:42 -0800

    implement linear time version of parse_freq from evernote challenge

--- python/evernote.py
+++ python/evernote.py
@@ -18,18 +18,43 @@ def parse_freq(doc, num):
 def parse_doc(doc):
 	return re.findall(r'[0-9a-zA-Z\'\-]+', doc.lower())
 
-
+"""Do what parse_freq does, but faster. This function runs in O(n) time
+versus using Counter's most_common method which does binary heap searching
+and runs at fastest, O(n + nlogk).
 """
-Notes on run-time:
+def parse_freq_better(doc, num):
+	# stuff I'll need later
+	f_dict = {}
+	f_max = 0
+	top_f = []
+	i = 0
+	j = 0
+	# map words to frequencies	
+	for word in parse_doc(doc): 
+		f_dict[word] = 1 if not (word in f_dict) else (f_dict[word]+1)
+	# initialize array using highest frequency 
+	for word in f_dict:
+		f_max = max(f_dict[word], f_max)
+	f_array = [None]*(f_max + 1)
+	# order words by frequency
+	for word in f_dict:
+		if f_array[f_dict[word]] == None:
+			f_array[f_dict[word]] = [word]
+		else:
+			f_array[f_dict[word]].append(word)
+	# extract the num words
+	f_array = f_array[::-1]
+	while num > 0:
+		try:
+			top_f.append(f_array[i][j])
+			num -= 1
+			j += 1
+		except (IndexError, TypeError):
+			j = 0
+			i += 1
+	return top_f
 
-It is impossible to write a solution that runs in O(n), where n is the 
-number of characters in the string. The fastest that a solution can be
-is O(nlog(k)) where n is the number of characters and k is the number
-of words to return in the final list.
-
-The python collections.Counter class creates and maintains an ordered
-dictionary in O(nlog(k)) time. Calling that class's most_common function
-actually runs python's heapq.nlargest(k) which runs in O(nlog(k)) time.
-The regex parsing runs in linear time. Altogether, parse_freq(doc, num)
-would run in O(nlog(k)) time.
-"""
+test_string = """Albert is my best friend. Albert loves me so much. Albert
+			   will always have my back because I love him like a brother.
+			   Brothers do everything with each other, including eat,
+			   sleep and shower."""
