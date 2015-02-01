ef42fc08e9525355be24756887ef01eb78177a06
Albert Hu <alberthu@berkeley.edu>
2015-01-31 18:01:16 -0800

    complete 2.1 and 2.2 LinkedList

--- python/LinkedList.py
+++ python/LinkedList.py
@@ -17,6 +17,9 @@ class Node:
                 p.next = n
                 p = p.next
 
+    def __str__(self):
+        return str(self.data)
+
     def appendToTail(self, data):
         end = Node(data)
         n = self
@@ -25,24 +28,67 @@ class Node:
         n.next = end
 
     @classmethod
-    def delete(cls, head, data):
+    def delete(cls, head, data=None):
 
-        # done if data is in first node
-        if head.data == data:
+        n = head
+
+        if n.data == data or not data:
             return head.next
 
-        # otherwise find the node to delete
-        pointer = head
-        while head.next.data != data:
-            pointer = pointer.next
+        while n.next:
+            if n.next.data == data:
+                n.next = n.next.next
+                return head
+            n = n.next
+        return head
 
-        # manipulate pointers to skip the node
-        skip = pointer.next.next
-        pointer.next.next = None
-        pointer.next = skip
 
-        # head is unchaged
-        return head 
+    def print_nodes(self):
+        bloop = list()
+        n = self
+        while n != None:
+            bloop.append("[" + str(n.data) + "]->")
+            n = n.next
+        bloop.append("None")
+        return str(''.join(bloop))
+
+# CTCI Chapter 2 coding questions
+
+# 2.1 Write code to remove duplicates from an unsorted linked list
+def remove_dups(node):
+    seen = {} 
+    result = list()
+    while node:
+        if node.data not in seen.keys():
+            seen[node.data] = True
+            result.append(node.data)
+        node = node.next
+    return Node(result)
+
+def remove_dups_sorted(node):
+    last = None
+    result = list()
+    while node:
+        if node.data != last:
+            last = node.data
+            result.append(node.data)
+        node = node.next
+    return Node(result)
+
+
+
+# 2.2 Implement an agorithm to find the kth to last element of a singly linked list
+def kth_to_last(node, k):
+    # use a runner
+    n, p = node, node
+    while k:
+        p = p.next
+        k -= 1
+    while p.next:
+        p = p.next
+        n = n.next
+    return n.next.data
+
 
 class TestNodeThings(unittest.TestCase):
 
@@ -65,10 +111,44 @@ class TestNodeThings(unittest.TestCase):
         self.assertEqual(n.next.next, None)
 
     def test_delete(self):
-        n = Node(1)
-        n.appendToTail(2)
-        n = Node.delete(n, 1)
-        self.assertEqual(n.data, 2)
+        one = Node(1)
+        one = Node.delete(one, 1)
+        self.assertEqual(one, None)
 
-if __name__ == "__main__":
+        two = Node([1, 2])
+        two = Node.delete(two, 2)
+        self.assertEqual(two.data, 1)
+        self.assertEqual(two.next, None)
+
+        three = Node([1, 2, 3])
+        three = Node.delete(three, 2)
+        self.assertEqual(three.next.data, 3)
+
+    def test_print_nodes(self):
+        single = Node(1)
+        self.assertEqual(single.print_nodes(), "[1]->None")
+
+        array = Node([1, 2, 3])
+        self.assertEqual(array.print_nodes(), "[1]->[2]->[3]->None")
+
+class TestInterviewQuestions(unittest.TestCase):
+
+    def test_remove_dups(self):
+        n = Node([1, 2, 2, 3])
+        n = remove_dups(n)
+        self.assertEqual(n.print_nodes(), Node([1, 2, 3]).print_nodes())
+
+    def test_remove_dups_sorted(self):
+        n = Node([1, 2, 2, 3])
+        n = remove_dups_sorted(n)
+        self.assertEqual(n.print_nodes(), Node([1, 2, 3]).print_nodes())
+
+    def test_kth_to_last(self):
+        n = Node([0, 1, 2, 3, 4, 5])
+        self.assertEqual(kth_to_last(n, 3), 3)
+
+    def test_kth_to_last_recursive(self):
+        n = Node([0, 1, 2, 3, 4, 5])
+
+if __name__ == '__main__':
     unittest.main()
