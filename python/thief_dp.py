591795758ee2a1646fcbeea46f9f37e22530b717
Albert Hu <alberthu@berkeley.edu>
2016-11-25 11:42:13 -0800

    Thief DP: use less space

--- python/thief_dp.py
+++ python/thief_dp.py
@@ -8,28 +8,30 @@
 
 # Memoize the max value up to the current house index
 def max_value_of_robbing(houses):
-    max_here = [0 for i in range(len(houses))]
-    max_here[0] = houses[0]
-    max_here[1] = max(houses[0], houses[1])
 
     # edge case
     if len(houses) < 3:
         return max(houses)
 
-    # either rob the current house or skip it
-    for i in range(1, len(houses)):
-        max_here[i] = max(max_here[i-1], max_here[i-2] + houses[i])
+    max_two_houses_ago = houses[0]
+    max_last_house = max(houses[0], houses[1])
 
-    return max_here[-1]
+    # either rob the current house or skip it
+    for i in range(2, len(houses)):
+        current_max = max(max_last_house, max_two_houses_ago + houses[i])
+        max_two_houses_ago = max_last_house
+        max_last_house = current_max
+
+    return current_max
 
 # We can use the max value to go backwards and find the indices
 def path_to_rob(houses, max_value):
     return
 
 # Tests
-houses = [1, 10, 10, 2]
-houses1= [10, 20]
-houses2= [10, 20, 30, 40, 50, 60, 1, 1, 1, 1, 100]
+houses = [1, 10, 10, 2] # 12
+houses1= [10, 20] # 20
+houses2= [10, 20, 30, 40, 50, 60, 1, 1, 1, 1, 100] # 221
 print(max_value_of_robbing(houses))
 print(max_value_of_robbing(houses1))
 print(max_value_of_robbing(houses2))
